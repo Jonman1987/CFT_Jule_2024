d0917796a3873d0805085155773e5685ce799d25
JonmanNote <Jonman@yandex.ru>
2024-07-13 17:03:06 +0700

    Work process 0.4

--- src/datatypesutility/view/UtilityView.java
+++ src/datatypesutility/view/UtilityView.java
@@ -1,4 +1,11 @@
 package datatypesutility.view;
 
+import datatypesutility.controller.Controller;
+
 public class UtilityView {
+    private final Controller controller;
+
+    public UtilityView(Controller controller) {
+        this.controller = controller;
+    }
 }
